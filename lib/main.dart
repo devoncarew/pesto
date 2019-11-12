7eac6b6a5ab5556e13eb2c0e79b07ebca0b8d815
Devon Carew <devoncarew@google.com>
2019-11-12 10:32:25 -0800

    commit //macos/

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,34 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'src/pesto.dart';
 
-void main() => runApp(PestoApp());
+void setOverrideForDesktop() {
+  if (kIsWeb) return;
+
+  if (Platform.isMacOS) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+  } else if (Platform.isLinux || Platform.isWindows) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.android;
+  } else if (Platform.isFuchsia) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+}
 
 class PestoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pesto',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        //platform: TargetPlatform.android,
+      ),
       home: PestoHome(),
     );
   }

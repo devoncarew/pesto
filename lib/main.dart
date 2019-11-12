68a203f5a3063e8b5f19edd850f7b1712081a642
Devon Carew <devoncarew@google.com>
2019-11-11 16:23:12 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+import 'pesto.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pesto',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: PestoHome(),
+    );
+  }
+}

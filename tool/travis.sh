c7953a16e01b5c3730c69d5e4f62c5b126771e97
Devon Carew <devoncarew@google.com>
2019-11-11 16:38:30 -0800

    install flutter

--- tool/travis.sh
+++ tool/travis.sh
@@ -3,6 +3,13 @@
 # Fast fail the script on failures.
 set -e
 
+export PATH=`pwd`/flutter/bin:`pwd`/flutter/bin/cache/dart-sdk/bin:$PATH
+flutter config --no-analytics
+flutter doctor
+
+# We should be using dart from ../flutter/bin/cache/dart-sdk/bin/dart.
+echo "which dart: " `which dart`
+
 # Verify that the libraries are error free.
 pub global activate tuneup
 pub global run tuneup check
